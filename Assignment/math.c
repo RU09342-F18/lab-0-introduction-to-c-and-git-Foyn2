ed637b27d7d41a766e87bd0d8b479208735b6042
Foyn2 <36171130+Foyn2@users.noreply.github.com>
2018-09-10 22:51:05 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -63,3 +63,65 @@ int math(int num1, int num2, char operator){
         break;
     }
 }
+
+/*The code below was the code input into the Coding Ground Compiler. In the Int main portion, the function 
+is being called 11 times to execute the 11 math operations*/
+
+#include <stdio.h>
+int answer = 0; //initializes the function
+
+int math(int num1, int num2, char operator); //math function created
+int main(void)//Main code
+{
+    /*The numbers 6 and 3 are picked as num1 and num2, respectively.
+    Each operand was put to the test with the same numbers*/
+        
+        math(6,3, '+');//Addition
+        printf("answer = %d \n", answer);//Printf is the way to display the output onscreen. Specifically, %d outputs an integer.
+        
+        math(6,3, '-');//Subtraction
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '*');//Multiplication
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '/');//Division
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '%');//Modulus
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '&');//Bitwise AND
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '|');//Bitwise OR
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '^');//Bitwise XOR
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '~');//Bitwise Negation
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '>');//Right Shift
+        printf("answer = %d \n", answer);
+        
+        math(6,3, '<');//Left Shift
+        printf("answer = %d \n", answer);
+} 
+
+//Here were the answers once the program was executed
+$gcc -o main *.c
+$main
+answer = 9 //(6+3)
+answer = 3 //(6-3)
+answer = 18 //(6*3)
+answer = 2 //(6/3)
+answer = 0 //(6%3)
+answer = 2 //(6&3)
+answer = 7 //(6|3)
+answer = 5 //(6^3)
+answer = -7 //(Negation)
+answer = 0 //(6>>3)
+answer = 48 //(6<<3)
+ 
